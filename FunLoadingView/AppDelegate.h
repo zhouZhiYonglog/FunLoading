0c9d0264f9bdc5a5d697fabe54f44a3b08c8a5df
zhouzhiyong <zhouzhiyong_work@163.com>
2019-01-08 10:13:30 +0800

    Initial Commit

---
+++ FunLoadingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunLoadingView
+//
+//  Created by 周智勇 on 2019/1/8.
+//  Copyright © 2019年 zhouzhiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunLoadingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunLoadingView
+//
+//  Created by 周智勇 on 2019/1/8.
+//  Copyright © 2019年 zhouzhiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
